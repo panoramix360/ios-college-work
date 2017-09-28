5f12935dcafc166c9f430e45bc2e2e2d7b63c0ea
System Administrator <root@iMac-de-Lucas.local>
2017-09-28 00:39:52 -0300

    Adding image view

---
+++ ios-game/ios-game/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ios-game
+//
+//  Created by Lucas de Oliveira Reis on 28/09/17.
+//  Copyright © 2017 Lucas de Oliveira Reis. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let urlBlankCard = "https://firebasestorage.googleapis.com/v0/b/ios-game-fdc0d.appspot.com/o/Back.jpg?alt=media&token=6c77d99d-39e6-4ff1-9353-8d250d55eb68"
+}
--- ios-game/ios-game/GameViewController.swift
+++ ios-game/ios-game/GameViewController.swift
@@ -16,19 +16,27 @@ class GameViewController: UIViewController {
     let gamesDb = Database.database().reference(withPath: "games")
     let cardsDb = Database.database().reference(withPath: "cards")
     
+    
+    
     var allCards = [Card]()
     var game: Game?
     
     @IBOutlet weak var gameName: UILabel!
     @IBOutlet weak var userRequesting: UILabel!
     @IBOutlet weak var userChallenging: UILabel!
+    @IBOutlet weak var round: UILabel!
+    @IBOutlet weak var scoreUserRequesting: UILabel!
+    @IBOutlet weak var scoreUserChallenging: UILabel!
+    
+    @IBOutlet weak var userRequestingImageCard: UIImageView!
+    @IBOutlet weak var userChallengingImageCard: UIImageView!
     
     @IBOutlet weak var gameStatus: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.gameName.text = self.game?.name
+        self.title = self.game?.name
         
         if self.game?.userChallenging != "" {
             self.getAllCards(game: self.game!)
@@ -59,6 +67,14 @@ class GameViewController: UIViewController {
         self.gamesDb.child((game.id)).updateChildValues(game.toAnyObject() as! [AnyHashable : Any])
     }
     
+    func updateGameOutlets(game: Game) {
+        self.userRequesting.text = (game.userRequesting)
+        self.userChallenging.text = (game.userChallenging)
+        self.round.text = String(game.round)
+        self.scoreUserRequesting.text = String(game.scoreUserRequesting)
+        self.scoreUserChallenging.text = String(game.scoreUserChallenging)
+    }
+    
     func getAllCards(game: Game) {
         // preenchendo todas as cartas
         self.cardsDb.observeSingleEvent(of: .value, with: { (snapshot) in
@@ -83,13 +99,13 @@ class GameViewController: UIViewController {
                 let gameItem = Game(snapshot: snapshot as! DataSnapshot)
                 if self.game?.userChallenging == "" && gameItem.userChallenging != "" {
                     self.gameStatus.text = "Jogo irá começar..."
+                    self.startGame()
                 } else if self.game?.userChallenging != "" && gameItem.userChallenging == "" {
                     self.gameStatus.text = "Desafiante saiu da sala..."
                 }
                 
                 self.game = gameItem
-                self.userRequesting.text = (self.game?.userRequesting)!
-                self.userChallenging.text = (self.game?.userChallenging)!
+                self.updateGameOutlets(game: self.game!)
             } else {
                 self.game = nil
                 self.navigationController?.popViewController(animated: true)
@@ -112,7 +128,33 @@ class GameViewController: UIViewController {
     }
     
     func sortFirstPlayer(game: Game) {
-        game.roundUser = arc4random_uniform(1) as! Int
+        game.roundUser = Int(arc4random_uniform(1))
+    }
+    
+    func startGame() {
+        loadImage(imageUrlString: Constants.urlBlankCard, view: self.userRequestingImageCard)
+        loadImage(imageUrlString: Constants.urlBlankCard, view: self.userChallengingImageCard)
+    }
+    
+    func loadImage(imageUrlString: String, view: UIImageView) {
+        let url = URL(string: imageUrlString)
+        
+        let task = URLSession.shared.dataTask(with: url!) {responseData,response,error in
+            if error == nil{
+                if let data = responseData {
+                    
+                    DispatchQueue.main.async {
+                        view.image = UIImage(data: data)
+                    }
+                }else {
+                    print("no data")
+                }
+            }else{
+                print(error)
+            }
+        }
+        
+        task.resume()
     }
     
     /*
